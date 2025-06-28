d873102ce851c0a22c2eb7881aa6013524789c45
Pranav Hydrabade <pranav2329h@gmail.com>
2025-06-28 14:21:10 +0530

    day2.1

--- demo/app.py
+++ demo/app.py
@@ -1,38 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///project.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
-
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=True)
     desc = db.Column(db.String(500), nullable=True)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f"<Todo {self.title} - {self.sno}>"
-
-# Route for home
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    if not Todo.query.first():
-        todo = Todo(title="First Todo", desc="Start investing in stock market")
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title=title, desc=desc)
         db.session.add(todo)
         db.session.commit()
+        return redirect('/')  
 
     allTodo = Todo.query.all()
-    return render_template('index.html', allTodo=allTodo)
+    return render_template('index.html',allTodo=allTodo)
 @app.route('/show')
 def products():
     allTodo = Todo.query.all()
     print(allTodo)
     return 'This is the product page'
+@app.route('/update')
+def update():
+    return 'This is the update page'
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    if todo:
+        db.session.delete(todo)
+        db.session.commit()
+    return redirect('/')
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  
+        db.create_all()
     app.run(debug=True)
